a42b996b3fbd7e09f38f8e86b1c13fb816070043
almeidasrenato <almeidasilvarenato@gmail.com>
2022-05-20 11:42:54 -0300

    primeira versão do todoList completo.

--- App.js
+++ App.js
@@ -1,21 +1,184 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  TextInput,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  //Pressable
+} from 'react-native'
+
+import uuid from 'react-native-uuid'
 
 export default function App() {
+  const [taskName, onChangeTaskName] = React.useState('')
+  const [task, onChangeTask] = React.useState([])
+
+  const addTask = (taskName) => {
+    let taskObject = {
+      id: uuid.v4(),
+      name: taskName,
+      complete: false,
+    }
+
+    onChangeTaskName('')
+    onChangeTask([...task, taskObject])
+  }
+
+  const removeTask = (taskId) => {
+    var newTaskList = task.filter((item) => item.id !== taskId)
+
+    onChangeTask(newTaskList)
+  }
+
+  const changeStatusTask = (taskId) => {
+    let newTaskList = task.map((item) => {
+      return {
+        ...item,
+        complete: taskId === item.id ? !item.complete : item.complete,
+      }
+    })
+
+    onChangeTask(newTaskList)
+  }
+
+  const renderTaskList = ({ item }) => (
+    <TouchableOpacity onPress={() => changeStatusTask(item.id)}>
+      <View style={styles.taskCard}>
+        {item.complete ? (
+          <Text style={styles.titleTaskCardLine}>{item.name}</Text>
+        ) : (
+          <Text style={styles.titleTaskCard}>{item.name}</Text>
+        )}
+
+        <Button
+          onPress={() => removeTask(item.id)}
+          title='Remover'
+          color='#F56D5B'
+        />
+      </View>
+    </TouchableOpacity>
+  )
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <StatusBar style='auto' />
+      <View style={styles.todoField}>
+        <Text style={styles.textTask}>Tarefas</Text>
+
+        {/* Field Tasks List */}
+        <View style={styles.taskListField}>
+          <FlatList
+            data={task}
+            renderItem={renderTaskList}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+
+        {/* Add Task Field */}
+        <View style={styles.addTaskField}>
+          <View style={styles.inputTextAndButtonField}>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeTaskName}
+              value={taskName}
+              placeholder='Digite a Tarefa'
+            />
+
+            <Button
+              onPress={() => addTask(taskName)}
+              title='Add'
+              color='#1499EF'
+              disabled={taskName === '' ? true : false}
+            />
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FAF6F5',
+    paddingTop: Platform.OS === 'android' ? 28 : 0,
   },
-});
+  todoField: {
+    flex: 1,
+    width: '100%',
+    alignItems: 'center',
+  },
+
+  // Field Tasks List
+
+  taskListField: {
+    flex: 1,
+    width: '100%',
+    padding: 32,
+  },
+  taskCard: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    minHeight: 60,
+    width: '100%',
+    padding: 10,
+    backgroundColor: '#fff',
+    borderColor: '#C9C9C9',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  titleTaskCard: {
+    flex: 1,
+  },
+  titleTaskCardLine: {
+    flex: 1,
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid',
+    textDecorationColor: '#000',
+  },
+
+  //Field addTask
+  textTask: {
+    fontSize: 34,
+    fontWeight: '500',
+    color: '#3E536B',
+    padding: 4,
+    paddingTop: 16,
+  },
+
+  addTaskField: {
+    padding: 32,
+  },
+  inputTextAndButtonField: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#fff',
+    borderColor: '#C9C9C9',
+    borderWidth: 1,
+    borderRadius: 8,
+
+    paddingRight: 10,
+
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.17,
+    shadowRadius: 3.05,
+    elevation: 4,
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    padding: 10,
+  },
+})
