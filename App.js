581bb414aa1618653c5c6b8ed75d0824f167a581
almeidasrenato <almeidasilvarenato@gmail.com>
2022-05-24 01:16:20 -0300

    Refatoração e melhora no código.

--- App.js
+++ App.js
@@ -1,219 +1,22 @@
-/* eslint-disable no-use-before-define */
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Platform,
-  TextInput,
-  FlatList,
-  TouchableOpacity,
-  KeyboardAvoidingView,
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, SafeAreaView, Platform } from "react-native";
 
-  // Pressable
-} from 'react-native';
-
-import uuid from 'react-native-uuid';
-
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import { SvgXml } from 'react-native-svg';
-import { addIcon, removeIcon, checkBoxDisabledIcon, checkBoxEnabledIcon } from './src/assets/svg';
+import Tasks from "./src/screens/tasks";
 
 export default function App() {
-  const [taskName, onChangeTaskName] = React.useState('');
-  const [task, onChangeTask] = React.useState([]);
-
-  // const [getStoreData, onChangeGetStoreData] = React.useState(false);
-
-  const storeData = async (value) => {
-    try {
-      const jsonValue = JSON.stringify(value);
-      await AsyncStorage.setItem('@taskList', jsonValue);
-    } catch (e) {
-      // saving error
-    }
-  };
-
-  const getData = async () => {
-    const jsonValue = await AsyncStorage.getItem('@taskList');
-
-    return onChangeTask(jsonValue != null ? JSON.parse(jsonValue) : []);
-  };
-
-  React.useEffect(() => {
-    getData();
-  }, []);
-
-  const addTask = async (taskNameProp) => {
-    const taskObject = {
-      id: uuid.v4(),
-      name: taskNameProp,
-      complete: false,
-    };
-
-    onChangeTaskName('');
-    onChangeTask([...task, taskObject]);
-
-    await storeData([...task, taskObject]);
-  };
-
-  const removeTask = async (taskId) => {
-    const newTaskList = task.filter((item) => item.id !== taskId);
-
-    onChangeTask(newTaskList);
-
-    await storeData(newTaskList);
-  };
-
-  const changeStatusTask = async (taskId) => {
-    const newTaskList = task.map((item) => ({
-      ...item,
-      complete: taskId === item.id ? !item.complete : item.complete,
-    }));
-
-    onChangeTask(newTaskList);
-    await storeData(newTaskList);
-  };
-
-  const renderTaskList = ({ item }) => (
-    <TouchableOpacity onPress={() => changeStatusTask(item.id)}>
-      <View style={styles.taskCard}>
-        {item.complete ? (
-          <SvgXml xml={checkBoxEnabledIcon()} width="30" height="30" />
-        ) : (
-          <SvgXml xml={checkBoxDisabledIcon()} width="30" height="30" />
-        )}
-
-        {item.complete ? (
-          <Text style={styles.titleTaskCardLine}>{item.name}</Text>
-        ) : (
-          <Text style={styles.titleTaskCard}>{item.name}</Text>
-        )}
-
-        <TouchableOpacity onPress={() => removeTask(item.id)}>
-          <SvgXml xml={removeIcon()} width="30" height="30" />
-        </TouchableOpacity>
-      </View>
-    </TouchableOpacity>
-  );
-
   return (
     <SafeAreaView style={styles.container}>
-      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : ''} style={{ flex: 1 }}>
-        <StatusBar style="auto" />
-
-        <View style={styles.todoField}>
-          <Text style={styles.textTask}>Tarefas</Text>
-
-          {/* Field Tasks List */}
-          <View style={styles.taskListField}>
-            <FlatList data={task} renderItem={renderTaskList} keyExtractor={(item) => item.id} />
-          </View>
-
-          {/* Add Task Field */}
-          <View style={styles.addTaskField}>
-            <View style={styles.inputTextAndButtonField}>
-              <TextInput
-                style={styles.input}
-                onChangeText={onChangeTaskName}
-                value={taskName}
-                placeholder="Digite a Tarefa"
-              />
-
-              <TouchableOpacity onPress={() => (taskName === '' ? null : addTask(taskName))}>
-                <SvgXml xml={addIcon(taskName === '')} width="30" height="30" />
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-      </KeyboardAvoidingView>
+      <StatusBar />
+      <Tasks />
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#FAF6F5",
     flex: 1,
-    backgroundColor: '#FAF6F5',
-    paddingTop: Platform.OS === 'android' ? 28 : 0,
-  },
-  todoField: {
-    flex: 1,
-    width: '100%',
-    alignItems: 'center',
-  },
-
-  // Field Tasks List
-
-  taskListField: {
-    flex: 1,
-    width: '100%',
-    padding: 32,
-  },
-  taskCard: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    minHeight: 60,
-    width: '100%',
-    padding: 10,
-    backgroundColor: '#fff',
-    borderColor: '#C9C9C9',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginTop: 10,
-  },
-  titleTaskCard: {
-    flex: 1,
-    paddingLeft: 8,
-    color: '#3E536B',
-  },
-  titleTaskCardLine: {
-    flex: 1,
-    paddingLeft: 8,
-    color: '#3E536B',
-    textDecorationLine: 'line-through',
-    textDecorationStyle: 'solid',
-    textDecorationColor: '#000',
-  },
-
-  // Field addTask
-  textTask: {
-    fontSize: 34,
-    fontWeight: '500',
-    color: '#3E536B',
-    padding: 4,
-    paddingTop: 16,
-  },
-
-  addTaskField: {
-    padding: 32,
-  },
-  inputTextAndButtonField: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    width: '100%',
-    backgroundColor: '#fff',
-    borderColor: '#C9C9C9',
-    borderWidth: 1,
-    borderRadius: 8,
-
-    paddingRight: 10,
-
-    shadowColor: '#000000',
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.17,
-    shadowRadius: 3.05,
-    elevation: 4,
-  },
-  input: {
-    flex: 1,
-    height: 40,
-    padding: 10,
+    paddingTop: Platform.OS === "android" ? 28 : 0,
   },
 });
---
+++ src/screens/tasks/index.js
@@ -0,0 +1,239 @@
+/* eslint-disable no-use-before-define */
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  // Pressable
+} from "react-native";
+
+import { SvgXml } from "react-native-svg";
+import {
+  addIcon,
+  removeIcon,
+  checkBoxDisabledIcon,
+  checkBoxEnabledIcon,
+} from "../../assets/svg";
+
+import uuid from "react-native-uuid";
+
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+function Tasks() {
+  const [taskName, onChangeTaskName] = React.useState("");
+  const [task, onChangeTask] = React.useState([]);
+
+  React.useEffect(() => {
+    getData();
+  }, []);
+
+  const storeData = async (value) => {
+    try {
+      const jsonValue = JSON.stringify(value);
+      await AsyncStorage.setItem("@taskList", jsonValue);
+    } catch (e) {
+      // saving error
+    }
+  };
+
+  const getData = async () => {
+    const jsonValue = await AsyncStorage.getItem("@taskList");
+
+    return onChangeTask(jsonValue != null ? JSON.parse(jsonValue) : []);
+  };
+
+  const addTask = async (taskNameProp) => {
+    const taskObject = {
+      id: uuid.v4(),
+      name: taskNameProp,
+      complete: false,
+    };
+
+    onChangeTaskName("");
+    onChangeTask([...task, taskObject]);
+
+    await storeData([...task, taskObject]);
+  };
+
+  const removeTask = async (taskId) => {
+    const newTaskList = task.filter((item) => item.id !== taskId);
+
+    onChangeTask(newTaskList);
+
+    await storeData(newTaskList);
+  };
+
+  const changeStatusTask = async (taskId) => {
+    const newTaskList = task.map((item) => ({
+      ...item,
+      complete: taskId === item.id ? !item.complete : item.complete,
+    }));
+
+    onChangeTask(newTaskList);
+    await storeData(newTaskList);
+  };
+
+  const TitleRender = () => (
+    <Text style={styleTitle.textTask}>{"Tarefas"}</Text>
+  );
+
+  const TaskListRender = () => {
+    const renderTaskListCard = ({ item }) => (
+      <TouchableOpacity onPress={() => changeStatusTask(item.id)}>
+        <View style={styleTaskList.taskCard}>
+          {item.complete ? (
+            <SvgXml xml={checkBoxEnabledIcon()} width="30" height="30" />
+          ) : (
+            <SvgXml xml={checkBoxDisabledIcon()} width="30" height="30" />
+          )}
+
+          {item.complete ? (
+            <Text style={styleTaskList.titleTaskCardLine}>{item.name}</Text>
+          ) : (
+            <Text style={styleTaskList.titleTaskCard}>{item.name}</Text>
+          )}
+
+          <TouchableOpacity onPress={() => removeTask(item.id)}>
+            <SvgXml xml={removeIcon()} width="30" height="30" />
+          </TouchableOpacity>
+        </View>
+      </TouchableOpacity>
+    );
+
+    return (
+      <View style={styleTaskList.taskListField}>
+        <FlatList
+          data={task}
+          renderItem={renderTaskListCard}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+    );
+  };
+
+  const AddTaskRender = () => {
+    const CustomButtonAddTask = () => (
+      <TouchableOpacity
+        onPress={() => (taskName === "" ? null : addTask(taskName))}
+      >
+        <SvgXml xml={addIcon(taskName === "")} width="30" height="30" />
+      </TouchableOpacity>
+    );
+
+    return (
+      <View style={styleAddTask.addTaskField}>
+        <View style={styleAddTask.inputTextAndButtonField}>
+          <TextInput
+            style={styleAddTask.input}
+            onChangeText={onChangeTaskName}
+            value={taskName}
+            placeholder="Digite a Tarefa"
+          />
+          <CustomButtonAddTask />
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : ""}
+      style={{ flex: 1 }}
+    >
+      <View style={styles.todoField}>
+        <TitleRender />
+        <TaskListRender />
+        <AddTaskRender />
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  todoField: {
+    alignItems: "center",
+    flex: 1,
+    width: "100%",
+  },
+});
+
+const styleTitle = StyleSheet.create({
+  textTask: {
+    color: "#3E536B",
+    fontSize: 34,
+    fontWeight: "500",
+    padding: 4,
+    paddingTop: 16,
+  },
+});
+
+const styleTaskList = StyleSheet.create({
+  taskListField: {
+    flex: 1,
+    padding: 32,
+    width: "100%",
+  },
+  taskCard: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderColor: "#C9C9C9",
+    borderRadius: 8,
+    borderWidth: 1,
+    flexDirection: "row",
+    marginTop: 10,
+    minHeight: 60,
+    padding: 10,
+    width: "100%",
+  },
+  titleTaskCard: {
+    color: "#3E536B",
+    flex: 1,
+    paddingLeft: 8,
+  },
+  titleTaskCardLine: {
+    color: "#3E536B",
+    flex: 1,
+    paddingLeft: 8,
+    textDecorationColor: "#000",
+    textDecorationLine: "line-through",
+    textDecorationStyle: "solid",
+  },
+});
+
+const styleAddTask = StyleSheet.create({
+  addTaskField: {
+    padding: 32,
+  },
+  inputTextAndButtonField: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderColor: "#C9C9C9",
+    borderRadius: 8,
+    borderWidth: 1,
+    elevation: 4,
+    flexDirection: "row",
+
+    paddingRight: 10,
+
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.17,
+    shadowRadius: 3.05,
+    width: "100%",
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    padding: 10,
+  },
+});
+
+export default Tasks;
